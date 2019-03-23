36a92b5d16a3b57479e946f51e839dac3b00152b
Nick Anderson <njcanderson@gmail.com>
2019-03-23 14:54:22 -0400

    setting up board service and creating a new board

--- src/app/board.service.ts
+++ src/app/board.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoardService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getNewBoardIdentifier(): Observable<string> {
+    return this.http.get<string>(environment.retroService + `/boards/new`);
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BoardService } from '../board.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private boardService: BoardService, private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  generateNewBoard() {
+    var guid = this.boardService.getNewBoardIdentifier();
+    this.router.navigateByUrl(`/board/${guid}`);
   }
-
 }
